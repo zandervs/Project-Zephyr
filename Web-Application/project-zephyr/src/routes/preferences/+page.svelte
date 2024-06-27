2ca840447b3d7831e837f205b63ae8615a5382f1
zandervs <zander.vschoor@gmail.com>
2024-06-27 14:54:13 +0200

    Broke everything by sneezing and somehow managed to fix it

---
+++ Web-Application/project-zephyr/src/routes/preferences/+page.svelte
@@ -0,0 +1,53 @@
+  <script lang="ts">
+    import UserProfile from 'clerk-sveltekit/client/UserProfile.svelte';
+    import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
+  </script>
+  
+  <style lang="postcss">
+    :global(body) {
+      margin: 0;
+      font-family: system-ui, sans-serif;
+      background-color: hsl(var(--background));
+      color: hsl(var(--foreground));
+    }
+  
+    .breadcrumb-container {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-top: 2rem;
+    }
+  
+    .breadcrumb-container a,
+    .breadcrumb-container span {
+      font-size: 50px; /* Apply the font size to anchor and span elements */
+    }
+  
+    .container {
+      display: flex;
+      justify-content: center;
+      align-items: flex-start; /* Ensure content starts from the top */
+      padding-top: 2rem;
+      padding-bottom: 6rem;
+    }
+  </style>
+  
+  <html lang="en-US" class="dark">
+    <div class="breadcrumb-container">
+      <Breadcrumb.Root>
+        <Breadcrumb.List>
+          <Breadcrumb.Item>
+            <Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Separator />
+          <Breadcrumb.Item>
+            <Breadcrumb.Page>User Settings</Breadcrumb.Page>
+          </Breadcrumb.Item>
+        </Breadcrumb.List>
+      </Breadcrumb.Root>
+    </div>
+    
+    <div class="container">
+      <UserProfile />
+    </div>  
+  </html>
